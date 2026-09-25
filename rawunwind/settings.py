30de85a925e7c7977beb5e91b1ec0ded72a8e29c
unknown <simonruth749@gmail.com>
2026-09-25 16:46:48 +0300

    update settings

--- rawunwind/settings.py
+++ rawunwind/settings.py
@@ -28,6 +28,7 @@ ALLOWED_HOSTS = [
     "localhost",
     "127.0.0.1",
     ".vercel.app",
+    "rawunwindsafaris.com"
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -114,17 +115,25 @@ WSGI_APPLICATION = "rawunwind.wsgi.application"
 # DATABASE
 # ============================================================
 
-DATABASE_URL = os.environ.get("DATABASE_URL")
+# DATABASE_URL = os.environ.get("DATABASE_URL")
+
+# DATABASES = {
+#     'default':
+#         dj_database_url.config(
+#         default=os.environ.get('DATABASE_URL'),
+#         conn_max_age=600,
+#         conn_health_checks=True,
+#         ssl_require=True,
+        
+#     )
+# }
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-        ssl_require=True,
-    )
-}
-
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # ============================================================
 # PASSWORD VALIDATION
 # ============================================================
